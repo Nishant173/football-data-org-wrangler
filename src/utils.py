2ba30438f25c7a47f14e573226c7608a1da54dff
Nishant173 <nishant.rao173@gmail.com>
2020-07-31 00:45:53 +0530

    cleaned file naming (from API endpoint)

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,82 @@
 import os
+import re
 import string
 import time
 import warnings
 import numpy as np
 import settings
 
-def truncate_after_special_char(endpoint_component):
-    special_chars = list(string.punctuation)
-    endpoint_component = str(endpoint_component).strip()
-    endpoint_component_truncated = ""
-    for char in endpoint_component:
-        if char not in special_chars:
-            endpoint_component_truncated += char
-        else:
-            break
-    return endpoint_component_truncated
+def identify_primary_resource(api_endpoint):
+    if 'scorers' in api_endpoint:
+        primary_resource = 'scorers'
+    else:
+        endpoint_components = api_endpoint.split('/')
+        endpoint_components = list(filter(None, endpoint_components))
+        endpoint_components.remove('v2')
+        primary_resource = endpoint_components[0]
+    return primary_resource
 
 
-def get_filename_from_endpoint(api_endpoint):
+def get_competition_code(api_endpoint):
     endpoint_components = api_endpoint.split('/')
     endpoint_components = list(filter(None, endpoint_components))
     endpoint_components.remove('v2')
-    cleaned_endpoint_components = []
-    for endpoint_component in endpoint_components:
-        truncated = truncate_after_special_char(endpoint_component=endpoint_component)
-        cleaned_endpoint_components.append(truncated)
-    filename_for_results = "_".join(cleaned_endpoint_components)
+    if endpoint_components[0] in ['competitions']:
+        competition_code = endpoint_components[1]
+        return competition_code
+    return ''
+
+
+def get_team_or_player_id(api_endpoint):
+    endpoint_components = api_endpoint.split('/')
+    endpoint_components = list(filter(None, endpoint_components))
+    endpoint_components.remove('v2')
+    if endpoint_components[0] in ['players', 'teams']:
+        id_required = endpoint_components[1]
+        return id_required
+    return ''
+
+
+def remove_special_chars(string):
+    return ''.join(char for char in string if char.isalnum())
+
+
+def get_querystring_parameters(api_endpoint):
+    """ Gets list of querystring parameters from the API endpoint """
+    qs_parameters = []
+    api_endpoint = api_endpoint.lower().strip()
+    qs_search_index = re.search(pattern=r'\?', string=api_endpoint)
+    if qs_search_index:
+        qs = api_endpoint[qs_search_index.span()[1]:]
+        if '&' in qs:
+            qs_components = qs.split('&')
+            for qs_component in qs_components:
+                qs_parameters.append(remove_special_chars(string=qs_component))
+        else:
+            qs_parameters.append(remove_special_chars(string=qs))
+    return qs_parameters
+
+
+def get_filename_from_endpoint(api_endpoint):
+    api_endpoint = api_endpoint.lower().strip()
+    resources_found = []
+    for resource in settings.VALID_API_RESOURCES:
+        resource = resource.lower().strip()
+        resource_exists = re.search(pattern=resource, string=api_endpoint)
+        if resource_exists:
+            resources_found.append(resource)
+    
+    if 'competitions' in resources_found:
+        additional_identifier = get_competition_code(api_endpoint=api_endpoint)
+    elif 'teams' in resources_found:
+        additional_identifier = get_team_or_player_id(api_endpoint=api_endpoint)
+    elif 'players' in resources_found:
+        additional_identifier = get_team_or_player_id(api_endpoint=api_endpoint)
+    
+    qs_params = get_querystring_parameters(api_endpoint=api_endpoint)
+    
+    api_endpoint_keywords = resources_found + [additional_identifier] + qs_params
+    filename_for_results = "_".join(api_endpoint_keywords)
     return filename_for_results
 
 
