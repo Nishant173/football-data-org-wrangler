4dac3612e9d733eef630679aee8a165816dbb820
Nishant173 <nishant.rao173@gmail.com>
2020-07-31 00:45:15 +0530

    updated default endpoints

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -7,4 +7,6 @@ API_ENDPOINTS = [
     "/v2/teams/86",
     "/v2/competitions/CL/scorers?limit=40",
     "/v2/competitions/PL/scorers?limit=40",
+    "/v2/competitions/BL1/matches?season=2018",
+    "/v2/competitions/BL1/matches?season=2019",
 ]
